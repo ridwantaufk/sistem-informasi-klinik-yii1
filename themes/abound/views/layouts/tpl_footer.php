804b992a608b77c696a9de0458d43deccb180eb9
Ridwan Taufik <122095756+ridwantaufk@users.noreply.github.com>
2023-03-18 08:36:01 +0700

    Add files via upload

---
+++ themes/abound/views/layouts/tpl_footer.php
@@ -0,0 +1,10 @@
+	<footer>
+	    <div class="subnav navbar navbar-inverse">
+	        <div class="navbar-inner">
+	            <div class="container">
+	                Didesain oleh Ridwan Taufik<br>
+	                <?php echo Yii::app()->name; ?>
+	            </div>
+	        </div>
+	    </div>
+	</footer>
